930760ffa1fc0dec8b37a38978bc51bee7c6f487
ryotana <dev@saku-lab.net>
2026-02-28 14:11:02 +0900

    add scripts for linux

--- roles/linux/default.rb
+++ roles/linux/default.rb
@@ -4,3 +4,12 @@
 ].each do |link|
   dotfile_link link
 end
+
+Dir.glob(File.expand_path("../files/bin/*", __FILE__)) do |bin|
+  link File.join(node[:userhome], "bin", File.basename(bin)) do
+    to bin
+    user node[:username]
+    force false
+    action :create
+  end
+end
