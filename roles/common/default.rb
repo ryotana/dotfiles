aefdc0370b41aaf6f5aa83a0eb267ca2aa4dbcdb
ryotana <dev@saku-lab.net>
2026-02-28 14:40:09 +0900

    add scripts for common

--- roles/common/default.rb
+++ roles/common/default.rb
@@ -24,3 +24,12 @@ include_cookbook "dotfiles"
 ].each do |link|
   dotfile_link link
 end
+
+Dir.glob(File.expand_path("../files/bin/*", __FILE__)) do |bin|
+  link File.join(node[:userhome], "bin", File.basename(bin)) do
+    to bin
+    user node[:username]
+    force false
+    action :create
+  end
+end
