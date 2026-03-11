2a0a0811c62845bf489a9f39e211952287548787
ryotana <dev@saku-lab.net>
2026-03-11 18:45:46 +0900

    add karabiner config

--- roles/darwin/default.rb
+++ roles/darwin/default.rb
@@ -1,5 +1,16 @@
+%W[
+  #{node[:userhome]}/tmp
+  #{node[:userhome]}/.config/karabiner
+].each do |dir|
+  directory dir do
+    owner node[:username]
+    group node[:usergroup]
+  end
+end
+
 %w[
   .config/fish/conf.d/darwin.fish
+  .config/karabiner/karabiner.json
 ].each do |link|
   dotfile_link link
 end
