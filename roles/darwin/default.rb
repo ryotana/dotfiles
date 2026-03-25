78e931d477218194eb1c364e7360ed5c7756fde4
ryotana <dev@saku-lab.net>
2026-03-25 21:38:48 +0900

    install Alacritty

--- roles/darwin/default.rb
+++ roles/darwin/default.rb
@@ -1,6 +1,8 @@
 %W[
   #{node[:userhome]}/tmp
   #{node[:userhome]}/.config/karabiner
+  #{node[:userhome]}/.config/alacritty
+  #{node[:userhome]}/.config/alacritty/themes
 ].each do |dir|
   directory dir do
     owner node[:username]
@@ -11,8 +13,14 @@ end
 %w[
   .config/fish/conf.d/darwin.fish
   .config/karabiner/karabiner.json
+  .config/alacritty/alacritty.toml
 ].each do |link|
   dotfile_link link
+end
+
+execute "git clone https://github.com/alacritty/alacritty-theme #{node[:userhome]}/.config/alacritty/themes" do
+  user node[:username]
+  not_if "test -d #{node[:userhome]}/.config/alacritty/themes/.git"
 end
 
 _aws_fragments = plugin_fragments(".aws/config.darwin.d")
