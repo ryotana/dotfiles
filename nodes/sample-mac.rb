e6dbb314b0a1d0015215f3cec9765fb0e820f9eb
ryotana <dev@saku-lab.net>
2026-07-08 19:52:46 +0900

    add node sample for mac

---
+++ nodes/sample-mac.rb
@@ -0,0 +1,14 @@
+node.reverse_merge!(
+  plugins: ["ei"],
+  cookbooks: ["homebrew", "tmux"],
+  brew: {
+    tap: ["daipeihust/tap"],
+    packages: [
+      "eza", "gawk", "ripgrep", "diff-so-fancy",
+      "git", "git-lfs", "gitleaks", "tig", "ghq",
+      "font-hackgen-nerd", "im-select", "fish", "fzf",
+      "awscli",
+    ],
+    cask_packages: ["karabiner-elements", "google-japanese-ime", "alfred", "bartender", "session-manager-plugin", "alacritty", "1password", "firefox", "vivaldi", "notion", "notion-calendar", "visual-studio-code", "claude"],
+  },
+)
